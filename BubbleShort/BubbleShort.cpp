eb13c62f68342d29bf99df7ad3d2b1a773ceb5dc
M S I <M S I@MSI>
2024-03-08 11:10:04 +0700

    prosedur bublesortaray

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -24,3 +24,19 @@ void input() {
 		cin >> a[i];
 	}
 }
+
+void bubbleSortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;
+	} while (pass <= n - 1);
+}
