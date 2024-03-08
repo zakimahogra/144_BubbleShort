312888ac39be900d9c0e04a28064d668703b71c0
M S I <M S I@MSI>
2024-03-08 11:01:49 +0700

    tampilkan input

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -19,5 +19,8 @@ void input() {
 	cout << "Masukan Elemen Array" << endl;
 	cout << "===================" << endl;
 
-
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
 }
