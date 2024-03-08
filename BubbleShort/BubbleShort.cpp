381d20ab894aff86377ffd17b86dba7ac49006e4
M S I <M S I@MSI>
2024-03-08 10:58:27 +0700

    input 2

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -4,3 +4,20 @@ using namespace std;
 int a[20];
 int n;
 
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
+
+
+}
